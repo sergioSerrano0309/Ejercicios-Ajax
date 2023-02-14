5c2262d9ff497c40fc9b22f0c7aac0fb6ef8ca4c
sergio_serrano <sergioserranorodriguez9@gmail.com>
2023-02-14 19:36:12 +0100

    ejercicio 1 terminado

---
+++ Ejercicio 1/ejer1.js
@@ -0,0 +1,33 @@
+function buscarPaises() {
+ 
+    let busqueda = document.getElementById("pais").value;
+      
+      let llamada = new XMLHttpRequest();
+      let url = "ejer1.php"; 
+      let params = "?busqueda=" + busqueda;
+
+      llamada.onreadystatechange = function () 
+      {
+      if (this.readyState === 4 && this.status === 200) 
+      {
+        let nombres = JSON.parse(this.responseText);
+        let p = document.getElementById("paises");
+        p.innerHTML = "";
+        for (let nombre of nombres) 
+        {
+          let text = document.createTextNode(nombre);
+          p.appendChild(text);
+          p.innerHTML += ", "
+          console.log(p.innerText)
+          }
+        }
+      }
+
+      // Indico que es una peticion POST
+      llamada.open("GET", url+params, true);
+      // Esta linea es necesaria en una peticion POST
+      llamada.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      llamada.send(params); // Le paso los parametros
+
+      
+  }
